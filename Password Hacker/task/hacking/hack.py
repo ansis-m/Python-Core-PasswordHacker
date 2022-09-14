d22225f585e4cbfa1daa749ae933133d2e236f6f
Ansis Maleckis <maleckisansis@gmail.com>
2022-09-14 10:42:59 +0300

    some progress in stage 4

--- Password Hacker/task/hacking/hack.py
+++ Password Hacker/task/hacking/hack.py
@@ -4,6 +4,7 @@ import socket
 import itertools
 import string
 import os
+import json
 
 def next_password(file):
 
@@ -12,11 +13,34 @@ def next_password(file):
         for combo in set(map(''.join, itertools.product(*zip(password.upper(), password.lower())))):
             yield combo
 
+def next_login(file):
+
+    logins = (line for line in file.read().splitlines())
+    for login in logins:
+        for combo in set(map(''.join, itertools.product(*zip(login.upper(), login.lower())))):
+            yield combo
+
+def get_login(file, new_socket):
+
+    iter = next_login(file)
+    login = "".join(next(iter))
+
+    while True:
+        dictionary = {"login" : login, "password" : " "}
+        json_str = json.dumps(dictionary)
+
+        new_socket.send(json_str.encode())
+        message = new_socket.recv(1024).decode()
+        if json.loads(message)["result"] != "Wrong login!":
+            print(json.loads(message)["result"])
+            return login
+        else:
+            login = "".join(next(iter))
 
 
 def main():
 
-    file = open("C:\\Users\\Ansis\\Desktop\\ChemScraper\\Password Hacker\\Password Hacker\\task\\hacking\\passwords.txt", "r")
+    file = open("C:\\Users\\Ansis\\Desktop\\ChemScraper\\Password Hacker\\Password Hacker\\task\\hacking\\logins.txt", "r")
     args = sys.argv
     if len(args) != 3:
         print("wrong number of arguments")
@@ -24,23 +48,17 @@ def main():
 
     new_socket = socket.socket()
     new_socket.connect((args[1], int(args[2])))
+    login = get_login(file, new_socket)
+    print("login:", login)
 
 
-    iter = next_password(file)
 
-    while True:
-        password = "".join(next(iter))
-        new_socket.send(password.encode())
-        message = new_socket.recv(1024)
-        if message.decode() == "Connection success!":
-            print(password)
-            break
-        if message.decode() == "Too many attempts":
-            print(message.decode())
-            break
+
+
 
     file.close()
     new_socket.close()
+    sys.exit(1)
 
 if __name__ == "__main__":
     main()
